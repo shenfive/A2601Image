4f6182b303d22652889354776698b3d548219024
Danny Shen <shenfive@gmail.com>
2026-01-21 09:49:41 +0800

    本機檔案

--- A2601Image/ViewController.swift
+++ A2601Image/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImagView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let path = Bundle.main.path(forResource: "a001", ofType: "jpg"){
+            theImagView.image = UIImage.init(contentsOfFile: path)
+        }
+        
     }
 
 
